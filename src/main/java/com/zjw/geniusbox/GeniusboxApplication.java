18834f33564d3bf8e66095bd4b0fe9eac821501e
kaka888970 <zoujiawei950@sina.com>
2018-06-30 16:10:55 +0800

    JD first project

---
+++ src/main/java/com/zjw/geniusbox/GeniusboxApplication.java
@@ -0,0 +1,12 @@
+package com.zjw.geniusbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeniusboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeniusboxApplication.class, args);
+	}
+}
